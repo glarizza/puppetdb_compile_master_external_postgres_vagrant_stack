af0e06835500da3a12aa96044880d20388739ed8
Gary Larizza <gary@puppetlabs.com>
2016-08-29 15:43:48 -0700

    Remove testing code
    This commit removes code that was used for testing purposes and should
    not have been committed

--- puppet_code/create_compile_master_pdb_node_group.pp
+++ puppet_code/create_compile_master_pdb_node_group.pp
@@ -1,6 +1,5 @@
   pe_node_group { 'PE Compile Master w/ PuppetDB':
-    #parent  => 'PE Infrastructure',
-    parent  => 'All Nodes',
+    parent  => 'PE Infrastructure',
     pinned  => ['compile-master-puppetdb'],
     classes => {
       'puppet_enterprise::profile::master' => { 'puppetdb_host' => '${fqdn}', 'puppetdb_port' => '8081' },
@@ -11,17 +10,11 @@
   }
 
   pe_node_group { 'PE Master Override' :
-    #parent  => 'PE Master',
-    parent  => 'All Nodes',
+    parent  => 'PE Master',
     pinned  => ['pe-mom'],
     classes => {
       'puppet_enterprise::profile::master' => { 'puppetdb_host' => ['pe-mom', 'compile-master-puppetdb'] },
     }
   }
 
-  pe_node_group { 'PE Postgres':
-    ensure             => 'present',
-    classes            => {'puppet_enterprise::profile::database' => {'max_connections' => 200}},
-    parent             => 'All Nodes',
-    pinned             => ['external-postgres'],
-  }
+
